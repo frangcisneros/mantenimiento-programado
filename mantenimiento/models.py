8124fca5fafeecb8611015c1bb83c76009e163c6
Francisco Cisneros <franbichir@gmail.com>
2025-03-28 00:48:51 -0300

    mejores modelos y un grafico

--- mantenimiento/models.py
+++ mantenimiento/models.py
@@ -3,20 +3,55 @@ from django.db import models
 
 
 class Maquina(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def __str__(self):
-        return self.nombre
+    id_maquina = models.AutoField(primary_key=True)
+    tipo = models.CharField(max_length=100)
+    id_ultima_tarea = models.ForeignKey(
+        "TareaMantenimiento",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="ultima_tarea_maquina",
+    )
 
 
 class TareaMantenimiento(models.Model):
-    maquina = models.ForeignKey(Maquina, on_delete=models.CASCADE)
-    fecha_programada = models.DateField()
-    descripcion = models.TextField()
+    id_tarea = models.AutoField(primary_key=True)
+    id_tipo_mantenimiento = models.ForeignKey(
+        "TipoMantenimiento",
+        on_delete=models.CASCADE,
+        related_name="tareas_mantenimiento",
+    )
+    id_maquina = models.ForeignKey(
+        "Maquina",
+        on_delete=models.CASCADE,
+        related_name="tareas_maquina",
+    )
+    id_pieza = models.ForeignKey(
+        "Pieza",
+        on_delete=models.CASCADE,
+        related_name="tareas_pieza",
+    )
+    id_encargado = models.ForeignKey(
+        "Encargado",
+        on_delete=models.CASCADE,
+        related_name="tareas_encargado",
+    )
+    fecha = models.DateField()
 
-    def __str__(self):
-        return f"Mantenimiento de {self.maquina} en {self.fecha_programada}"
+
+class Pieza(models.Model):
+    id_pieza = models.AutoField(primary_key=True)
+    id_maquina = models.ForeignKey(
+        "Maquina",
+        on_delete=models.CASCADE,
+        related_name="pieza_maquina",
+    )
+
+
+class TipoMantenimiento(models.Model):
+    id_tipo_mantenimiento = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=100)
+
+
+class Encargado(models.Model):
+    id_encargado = models.AutoField(primary_key=True)
